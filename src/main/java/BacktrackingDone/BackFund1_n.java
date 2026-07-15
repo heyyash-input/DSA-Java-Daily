36038ba0f0fe409d0d599b10bdbcb87370f11f3d
heyyash-input <yashpatil8930@gmail.com>
2026-07-15 16:33:04 +0530

    feat: sudoku practice

--- src/main/java/BacktrackingDone/BackFund1_n.java
+++ src/main/java/BacktrackingDone/BackFund1_n.java
@@ -10,11 +10,11 @@ public class BackFund1_n {
 //        arrBack(arr, 0 , 1 );
 //        printArr(arr);
 
-//        findSubset("abc" , "" , 0);
-//         findPerm("abc" , "");
+        /// findSubset("abc" , "" , 0);
+        /// findPerm("abc" , "");
 
 //--------------------------------------------------------------------------------------------------------------
-        // NQueens:
+        /// NQueens:
 //        int n = 4;
 //        char board [] [] = new char[n][n] ;
 //
@@ -27,11 +27,43 @@ public class BackFund1_n {
 //        nQueens(board , 0 );
 //        System.out.println("total ways: " + count);
 //--------------------------------------------------------------------------------------------------------------
-        //Grid Path:
-        int n= 3 , m = 3 ;
-        System.out.println(gridPath(0  , 0 ,n  , m));
+///        Grid Path:
+//        int n= 3 , m = 3 ;
+//        System.out.println(gridPath(0  , 0 ,n  , m));
+//------------------------------------------------------------------------------------------------------------
+///         Soduku:-
+
+            int sudoku[][] = {
+                    {0, 0, 8, 0, 0, 0, 0, 0, 0},
+                    {4, 9, 0, 1, 5, 7, 0, 0, 2},
+                    {0, 0, 3, 0, 0, 4, 1, 9, 0},
+                    {1, 8, 5, 0, 6, 0, 0, 2, 0},
+                    {0, 0, 0, 0, 2, 0, 0, 6, 0},
+                    {9, 6, 0, 4, 0, 5, 3, 0, 0},
+                    {0, 3, 0, 0, 7, 2, 0, 0, 4},
+                    {0, 4, 9, 0, 3, 0, 0, 5, 7},
+                    {8, 2, 7, 0, 0, 9, 0, 1, 3}
+            };
+
+            if(sudokuSolver(sudoku , 0 , 0 )){
+                System.out.println("Solution exist");
+                printSudoku(sudoku);
+            }else{
+                System.out.println("Solution does not exist");
+            }
 
     }
+//---------------------------------------------------------------------------------------------------------------
+
+    public static void printSudoku(int sudoku [] []){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoku[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
 //--------------------------------------------------------------------------------------------------------------------
 
     public static void printArr(int arr []){
@@ -169,5 +201,69 @@ public class BackFund1_n {
         // TC:- O(2^m*n)
         // using permutation formula we can convert it to - O(m*n)
     }
+
+//---------------------------------------------------------------------------------------------------------------
+
+        /// Soduku:-
+        public static boolean sudokuSolver( int sudoku [] [] , int row , int col ){
+
+            // base case:
+            if(row == 9  ){
+                return true;
+            }
+            //recursion
+            int nextRow = row , nextCol = col+ 1 ;
+
+            if(col+1 == 9){
+                nextRow = row + 1 ;
+                nextCol = 0 ;
+            }
+
+            // don't change '0' values:
+            if( sudoku[row][col] != 0){
+                return sudokuSolver(sudoku , nextRow ,nextCol) ;
+            }
+
+            for (int digit = 1 ; digit <= 9; digit++) {
+                if(isSafe(sudoku , row , col , digit)){
+                    sudoku[row][col] = digit ;
+                    if (sudokuSolver(sudoku ,nextRow , nextCol )){
+                        return true ; // soln exit
+                    }
+                sudoku[row] [col] = 0 ; // Place zero
+                }
+            }
+            return false ;
+        }
+
+        public static boolean isSafe (int sudoku[] [] , int row , int col , int digit){
+
+            //column:
+            for (int i = 0; i <= 8 ; i++) {
+                if(sudoku[i][col] == digit){
+                    return false ;
+                }
+            }
+            // for row:
+            for (int j = 0; j <= 8 ; j++) {
+                if(sudoku[row][j] == digit){
+                    return false;
+                }
+            }
+
+            //grid
+            int sr = (row/3) * 3 ;
+            int sc = (col/3) * 3 ;
+            // Grid 3X3
+            for (int i = sr; i <sr + 3  ; i++) {
+                for (int j = sc; j < sc + 3 ; j++) {
+                    if (sudoku[i][j] == digit){
+                        return false ;
+                    }
+                }
+            }
+            return true;
+        }
+//--------------------------------------------------------------------------------------------------------------
 
 }
