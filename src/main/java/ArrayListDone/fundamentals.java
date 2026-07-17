45d2aaef3a79300b3e975df9802196cee542688d
heyyash-input <yashpatil8930@gmail.com>
2026-07-17 14:32:09 +0530

    feat: Pair sum variations solved

--- src/main/java/ArrayListDone/fundamentals.java
+++ src/main/java/ArrayListDone/fundamentals.java
@@ -65,8 +65,37 @@ public class fundamentals {
         System.out.println("Maximum water container can store: " + result);
         // Expected Output: 49
 
-    }
+//-------------------------------------------------------------------------------------------------------------
 
+        ///Pair sum:-
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        list.add(6);
+//        int target = 5 ;
+//        System.out.println(pairSum1(list, target));
+
+//----------------------------------------------------------------------------------------------------------------
+
+        ///Pair sum 2 :-
+        ArrayList<Integer> list = new ArrayList<>();
+        // [11 , 15 , 6 , 8 , 9 , 10 ] --> Roted and sorted
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16 ;
+        System.out.println(pairSum2(list, target));
+
+    }
+//---------------------------------------------------------------------------------------------------------------
+
+    /// Container with most water:-
     public static int containerWithMostWater(int height []) {
 
         int maxArea =0 ;
@@ -90,4 +119,79 @@ public class fundamentals {
         }
         return maxArea;
     }
+
+//--------------------------------------------------------------------------------------------------------------
+   /// brute force:-
+//    public static boolean pairSum1(ArrayList<Integer> list , int target){
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            for (int j = i+1; j < list.size(); j++) {
+//                if(list.get(i) + list.get(j) == target){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    ///TwoPointer:
+    public static boolean pairSum1(ArrayList<Integer> list , int target){
+        int left = 0 , right = list.size()-1;
+        while(left < right){
+            //case 1:
+            if(list.get(left) + list.get(right) == target){
+                return true ;
+            }
+
+            //case 2:
+             if (list.get(left) + list.get(right) < target ) {
+                left++;
+            }
+             //case 3:
+                else{
+                right -- ;
+            }
+
+        }
+        return false;
+    }
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+    /// pair sum 2 :-
+    public static boolean pairSum2(ArrayList<Integer> list , int target){
+
+        //breaking point pivot:-
+        int bp = -1 ;
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            if (list.get(i) > list.get(i+1)) {
+                //breaking point we got
+                bp = i ;
+                break;
+            }
+        }
+
+        int left = bp + 1 ; // smallest
+        int right = bp ; // largest
+
+        while (left != right) {
+
+            if (list.get(left) + list.get(right) == target) {
+                return true;
+            }
+
+            //Important pointer technique BS involved:-
+            if (list.get(left) + list.get(right) < target) {
+                left = (left + 1) % n;
+            } else {
+                right = (n + right - 1) % n;
+            }
+        }
+        return false;
+    }
+//---------------------------------------------------------------------------------------------------------------
+
+
 }
