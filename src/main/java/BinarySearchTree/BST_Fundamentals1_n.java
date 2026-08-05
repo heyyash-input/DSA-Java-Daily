cb3042edeeb9223863d11e4d53c3273e3e61eb73
heyyash-input <yashpatil8930@gmail.com>
2026-08-05 12:00:55 +0530

    feat: size of largest BST in BT VVIMP

--- src/main/java/BinarySearchTree/BST_Fundamentals1_n.java
+++ src/main/java/BinarySearchTree/BST_Fundamentals1_n.java
@@ -86,18 +86,41 @@ public class BST_Fundamentals1_n {
 //        Node root = sortedArrToBST(arr ,0 ,arr.length-1);
 //        preorder(root);
 
-        ///  Convert BST to Balanced BST:-
-        Node root = new Node(8);
-        root.left = new Node(6);
+//        ///  Convert BST to Balanced BST:-
+//        Node root = new Node(8);
+//        root.left = new Node(6);
+//        root.left.left = new Node(5);
+//        root.left.left.left = new Node(3);
+//
+//        root.right = new Node(10);
+//        root.right.right = new Node(11);
+//        root.right.right.right = new Node(12);
+//
+//       root =  bstToBal(root);
+//        preorder(root);
+
+        /// Maximum Size of Valid BST:-
+        Node root = new Node(50);
+        root.left = new Node(30);
         root.left.left = new Node(5);
-        root.left.left.left = new Node(3);
+        root.left.right = new Node(20);
 
-        root.right = new Node(10);
-        root.right.right = new Node(11);
-        root.right.right.right = new Node(12);
-
-       root =  bstToBal(root);
-        preorder(root);
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
+        /*
+               50
+             /    \
+           30      60
+          /  \    /  \
+         5   20  45   70
+                     /  \
+                    65   80
+         */
+        Info info = largestBST(root) ;
+        System.out.println("Largest BST size is " + maxBST);
 
     }
 //-------------------------------------------------------------------------------------------------------------
@@ -329,22 +352,23 @@ public class BST_Fundamentals1_n {
         getInorder(root , inorder);
 
         //sorted inorder -> balanced
-
         root = createBST(inorder , 0 , inorder.size()-1);
         return root ;
     }
 
     public static Node createBST(ArrayList<Integer> inorder , int start , int end){
+
         if(start > end){
             return null ;
         }
+
         int mid = start + (end - start) /2 ;
         Node root = new Node(inorder.get(mid));
         root.left = createBST(inorder , start , mid -1  );
-        root.right = createBST(inorder ,mid+1 , end );
-
+        root.right = createBST(inorder , mid + 1 , end);
         return root ;
     }
+
     public static void getInorder(Node root , ArrayList<Integer> inorder) {
         if(root == null){
             return;
@@ -352,6 +376,50 @@ public class BST_Fundamentals1_n {
         getInorder(root.left ,inorder);
         inorder.add(root.data );
         getInorder(root.right ,inorder);
+    }
+
+//---------------------------------------------------------------------------------------------------------------
+
+    /// Maximum Size of Valid BST:-
+    static class Info{
+        boolean isBST ;
+        int size;
+        int min;
+        int max;
+
+        public Info(Boolean isBST , int size , int min , int max ){
+            this.isBST = isBST ;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static int maxBST =0 ;
+
+    public static Info largestBST(Node root){
+
+        // edge case for null condition:-
+        if(root == null ){
+            return new Info(true ,0  , Integer.MAX_VALUE , Integer.MIN_VALUE);
+        }
+
+        // get all the information from left and right:-
+        Info leftInfo = largestBST(root.left);
+        Info rightInfo = largestBST(root.right);
+        int size = leftInfo.size + rightInfo.size + 1 ;
+        int min = Math.min(root.data  , Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data  , Math.max(leftInfo.max, rightInfo.max));
+
+        //calculate isBST:-
+        if(root.data  <= leftInfo.max || root.data >= rightInfo.min){
+            return new Info(false , size , min , max);
+        }
+        if(leftInfo.isBST && rightInfo.isBST ){
+            maxBST =  Math.max(maxBST , size);
+            return new Info(true , size , min , max);
+        }
+        return new Info(false , size , min , max) ;
     }
 
 //---------------------------------------------------------------------------------------------------------------
