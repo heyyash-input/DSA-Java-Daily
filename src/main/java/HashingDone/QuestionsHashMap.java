fe36e71e21ae1ae19642568420bcdcfbb0b290be
heyyash-input <yashpatil8930@gmail.com>
2026-08-06 15:54:44 +0530

    feat: practice questions

--- src/main/java/HashingDone/QuestionsHashMap.java
+++ src/main/java/HashingDone/QuestionsHashMap.java
@@ -16,7 +16,12 @@ public class QuestionsHashMap {
 
         /// Count Distinct:-
         int dist [] = { 4,3,2,5,6,7,3,4,2,1};
+        distinct(dist);
 
+        /// Union and Intersection:-
+        int arr1 [] = {7 , 3 , 9 };
+        int arr2 [] = { 6 ,3 ,9 ,2 , 9 , 4 };
+        uniAndInter(arr1 , arr2);
     }
 
 //------------------------------------------------------------------------------------------------------------------------------
@@ -69,6 +74,45 @@ public class QuestionsHashMap {
 //------------------------------------------------------------------------------------------------------------------------------
 
     /// Count Distinct:-
+    public static void distinct(int dist []) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < dist.length; i++) {
+            set.add(dist[i]);
+        }
+        System.out.println("unique elements are " + set.size());
+    }
 
+//------------------------------------------------------------------------------------------------------------------------------
 
+    /// Union and intersection:-
+
+    public static void uniAndInter(int arr1 [] , int arr2 []){
+        HashSet<Integer> set = new HashSet<>();
+
+        ///union:
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+        System.out.println("union=" + set.size());
+
+    ///----------------------------------------------------------------------------------
+        /// intersection:
+        set.clear();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        int count =0 ;
+        for (int i = 0; i < arr2.length ; i++) {
+            if(set.contains(arr2[i])){
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        System.out.println("My Intersection:" + count);
+
+    }
+//-------------------------------------------------------------------------------------------------------------------------
 }
