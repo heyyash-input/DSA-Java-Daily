3ea2af133b012a105fdfa1b891b5e925746bfdfb
heyyash-input <yashpatil8930@gmail.com>
2026-08-14 15:45:58 +0530

    feat: Prims Algo

--- src/main/java/Graphs/IMP_Algo.java
+++ src/main/java/Graphs/IMP_Algo.java
@@ -16,6 +16,8 @@ public class IMP_Algo {
         }
     }
 //---------------------------------------------------------------------------------------------------------------------------
+
+    // graph for Djkstra:
     static void createGraph(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
@@ -36,6 +38,7 @@ public class IMP_Algo {
     }
 //---------------------------------------------------------------------------------------------------------------------------
 
+    /// Graph for Bellman Ford Algo:-
     //Edge[]
     static void createGraphBell(ArrayList<Edge> graph[]) {
         for(int i=0; i<graph.length; i++) {
@@ -53,6 +56,26 @@ public class IMP_Algo {
 
         graph[4].add(new Edge(4, 1, -1));
     }
+//--------------------------------------------------------------------------------------------------------------------------
+
+     /// Graph for Prim's Algo:-
+     static void createGraphPrim(ArrayList<Edge> graph[]) {
+         for (int i = 0; i < graph.length; i++) {
+             graph[i] = new ArrayList<>();
+         }
+         graph[0].add(new Edge(0, 1, 10));
+         graph[0].add(new Edge(0, 2, 15));
+         graph[0].add(new Edge(0, 3, 30));
+
+         graph[1].add(new Edge(1, 0, 10));
+         graph[1].add(new Edge(1, 3, 40));
+
+         graph[2].add(new Edge(2, 0, 15));
+         graph[2].add(new Edge(2, 3, 50));
+
+         graph[3].add(new Edge(3, 1, 40));
+         graph[3].add(new Edge(3, 2, 50));
+     }
 
 //---------------------------------------------------------------------------------------------------------------------------
 
@@ -65,10 +88,11 @@ public class IMP_Algo {
                 dist[i] = Integer.MAX_VALUE;
             }
         }
-        //algo
+        //O(V X E):-
+        //algo :
         int V = graph.length;
         for (int i = 0; i < V - 1; i++) {
-            // edges - O(E):
+            // edges - O(E): bacause bellow loops takes edge time
             for (int j = 0; j < graph.length; j++) {
                 for (int k = 0; k < graph[j].size(); k++) {
                     Edge e = graph[j].get(k);
@@ -88,8 +112,49 @@ public class IMP_Algo {
         }
         System.out.println();
     }
+//---------------------------------------------------------------------------------------------------------------------------
+
+    /// Prim's Algo:- MST (Minimum spanning Tree)
+    public static void primAlgo(ArrayList<Edge> [] graph ){
+        boolean vis [] = new boolean[graph.length];
+        PriorityQueue<pair1> pq = new PriorityQueue<>();
+
+        pq.add(new pair1(0,0));
+
+        int finalCost = 0 ; //MST total min weight:
+
+        while(!pq.isEmpty()){
+            pair1 curr = pq.remove();
+            if(!vis[curr.v]){
+                vis[curr.v] = true;
+                finalCost += curr.cost;
+
+                for (int i = 0; i <graph[curr.v].size() ; i++) {
+                    Edge e = graph[curr.v].get(i);
+                    pq.add(new pair1(e.dest , e.wt));
+                }
+            }
+        }
+        System.out.println(finalCost+" ");
+    }
+
+    static class pair1 implements Comparable<pair1> {
+        int v;
+        int cost;
+
+        public pair1(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(pair1 p2) {
+            return this.cost - p2.cost;   // Ascending order (Min-Heap)
+        }
+    }
 
 //---------------------------------------------------------------------------------------------------------------------------
+
+    /// Dijkstra Algo:- O((V + E) \log V)
     // Pair class implementing Comparable for PriorityQueue sorting
     static class pair implements Comparable<pair> {
         int n;
@@ -144,6 +209,7 @@ public class IMP_Algo {
         }
         System.out.println();
     }
+
 //---------------------------------------------------------------------------------------------------------------------------
     public static void main(String args[]) {
 
@@ -152,13 +218,19 @@ public class IMP_Algo {
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
         int src = 0;
-        dijkstra(graph, src);
+        dijkstra(graph, src);//0 2 3 8 6 9
 
         /// Bellman Ford:-
         int Vbell = 6;
         ArrayList<Edge> graphbell[] = new ArrayList[Vbell];
         createGraphBell(graphbell);
-        BellmanFord(graphbell , 0);
+        BellmanFord(graphbell , 0);//0 2 -2 0 4
+
+        /// Prims Algo:-
+        int Vprim = 4 ;
+        ArrayList<Edge> graphprim[] = new ArrayList[Vprim];
+        createGraphPrim(graphprim);
+        primAlgo(graphprim); //55
     }
 //---------------------------------------------------------------------------------------------------------------------------
 }
